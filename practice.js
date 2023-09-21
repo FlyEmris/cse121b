d552cf889f32bc4e0997efe604921fcdb2fde141
JonathanLeonardson <jonnyislenny@gmail.com>
2023-09-21 13:03:05 -0600

    fixed my errors and made stuff work.

---
+++ practice.js
@@ -0,0 +1,7 @@
+const section = document.createElement('section');
+const h3 = document.createElement('h3');
+h3.textContent = 'Relief Society';
+
+console.log('${h3} ${section}.');
+
+const ward = '<strong>Home Ward</strong>: ${homeWard}';
---
+++ scripts/w02-task.js
@@ -0,0 +1,37 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = 'Jonathan Leonardson';
+let currentYear = '2023';
+let profilePicture = 'images/FB_IMG_1600378235339 (2).jpg';
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('main#home picture img')
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}<strong>`;
+textContent = 'currentYear';
+imageElement.setAttribute('src' , profilePicture);
+imageElement.setAttribute
+
+
+/* Step 5 - Array */
+
+let favoriteFood = [
+    "Pizza",
+    "Popcorn",
+    "Macaroni and Cheese"
+];
+
+let singleFavoriteFood = "oreo";
+favoriteFood.push(singleFavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+
