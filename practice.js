a13ccfea1fcd4fad357a7072cb1db99efec6f929
JonathanLeonardson <jonnyislenny@gmail.com>
2023-09-21 16:11:48 -0600

    got rid of clutter

--- practice.js
+++
@@ -1,8 +0,0 @@
-const section = document.createElement('section');
-const h3 = document.createElement('h3');
-h3.textContent = 'Relief Society';
-
-console.log('${h3} ${section}.');
-
-const ward = `<strong>Home Ward</strong>: ${homeWard}`;
-section.append(h3);
