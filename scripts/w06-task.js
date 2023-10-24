064ff213bb2bb6f7b695b79436a04eab03aa137f
JonathanLeonardson <jonnyislenny@gmail.com>
2023-10-23 20:12:27 -0600

    trying to fix

--- scripts/w06-task.js
+++ scripts/w06-task.js
@@ -5,7 +5,6 @@ const api_url = "https://api.themotivate365.com/stoic-quote";
 async function getquote(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
     quote.innerHTML = data.quote;
     author.innerHTML = data.author
 
